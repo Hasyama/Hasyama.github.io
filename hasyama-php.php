90e0973a47f039ec82ca8ed85d174ea6d54ee9fa
Hasyama <xasyama@gmail.com>
2016-07-22 16:30:30 +0300

    Add files via upload

---
+++ hasyama-php.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hello World</h1>
+<p>I'm hosted with GitHub Pages.</p>
+<?php 
+$txt = "GitHub";
+$str ="W3Schools"
+echo "I love both of em " . $txt . " and " . $str .  "!"?>
+</body>
+</html>
